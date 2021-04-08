da003bd535db23e133cd8ebbf65d2ba1d9174219
brettin <brettin@anl.gov>
2021-04-07 19:17:23 -0500

    spawn run_qc

--- zeromq/test/get_new_files.py
+++ zeromq/test/get_new_files.py
@@ -3,10 +3,13 @@
 #   Binds REP socket to tcp://*:5555
 #   Expects b"Hello" from client, replies with b"World"
 #
-
+import os
 import time
 import json
 import zmq
+from subprocess import Popen
+from run_qc import run_qc
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
@@ -16,16 +19,18 @@ while True:
     message = socket.recv()
     decoded = message.decode('utf-8')
     print("Received message\n",
-        json.dumps(json.loads(decoded), indent=4, sort_keys=True)
-        )
+            json.dumps(json.loads(decoded), indent=4, sort_keys=True)
+            )
+
+    #  Send reply back to client
+    socket.send(b"World")
 
     #  Do some 'work'
-    time.sleep(1)
+    child_pid = Popen(["python", "./run_qc.py", "myarg"],
+            start_new_session=True
+            ).pid
 
     # Shutdown if message is SHUTDOWN
     if message == b"SHUTDOWN":
         socket.send(b"Shutting down")
         break
-
-    #  Send reply back to client
-    socket.send(b"World")
