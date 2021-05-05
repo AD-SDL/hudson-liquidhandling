215f892fc74ba1cce8b8b7192a1b5032c5e73a11
brettin <brettin@anl.gov>
2021-05-05 16:55:20 -0500

    zmq connect util

---
+++ zeromq/utils/zmq_connection.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+import zmq
+
+'''
+    A collection of methods helpful in creating and
+    managing connections to zmq sockets
+'''
+
+def connect(port=None, pattern='REQ'):
+    ''' 
+    Connects to a queue on a given port using a 
+    given communication pattern
+    
+
+    returns a zmq context, zmq socket
+
+    '''
+
+    context = zmq.Context()
+    if pattern == 'REQ':
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://localhost:"+str(port))
+
+    return context, socket
