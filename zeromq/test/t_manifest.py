9250e90b26bbfa94cde66786f7136c8b1cafae96
brettin <brettin@anl.gov>
2021-05-05 20:58:37 -0500

    added purpose

--- zeromq/test/t_manifest.py
+++ zeromq/test/t_manifest.py
@@ -4,5 +4,5 @@ sys.path.append(".")
 
 from manifest import generateFileManifest
 
-text = generateFileManifest("manifest.py", manifest_filename="manifest.txt")
+text = generateFileManifest("manifest.py", purpose="build_dataframe", manifest_filename="manifest.txt")
 print(text)
