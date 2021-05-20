587cc1f2ccc39c6d2f3af167095b4907694bd24f
brettin <brettin@anl.gov>
2021-05-20 08:03:11 -0500

    standardizing handlers

--- zeromq/lambda6_handle_message_5556.py
+++ zeromq/lambda6_handle_message_5556.py
@@ -7,38 +7,72 @@
 
 import os
 import sys
+import inspect
 import json
 from path import Path
+from utils.zmq_connection import zmq_connect
 from utils.build_dataframe import build_dataframe
+from utils.manifest import generateFileManifest
+
+def _do_work(filenames):
+    new_filenames = build_dataframe(filenames)
+
+    if len(new_filenames) > 0:
+        multi_file_manifest = {}
+        context, socket = zmq_connect(port=5557, pattern="REQ")
+        for f in new_filenames:
+            single_file_manifest  = generateFileManifest(f, purpose="train_model")
+            for k in single_file_manifest:
+                multi_file_manifest[k] = single_file_manifest[k]
+
+        socket.send_string(json.dumps(multi_file_manifest))
+        repl = socket.recv()
+        print(f"Got {repl}")
+    else:
+        print("new_filenames is empty")
+        n = inspect.stack()[0][3]
+        print(f"{n} failed on {file_name}")
+
+    return new_filenames
 
 
-def lambda6_handle_message(decoded_message):
+def lambda6_handle_message(json_string):
 
     lambda6_data_path = "/lambda_stor/data/hudson/data/"
 
     # * extract message address and body
-    json_decoded = json.loads(decoded_message)
+    json_decoded = json.loads(json_string)
 
     return_val = "PASS"
     print(f"Handling message: {json_decoded}")
 
-    print(json_decoded)
+    filenames = []
     for k in json_decoded:
         file_data = json_decoded[k]
-        filename = file_data["path"]
-        print(f"filename {filename[0]}")
-        df = build_dataframe(filename)
-        print(f"data frame basename {df}")
+        filename = file_data["path"][0]
+        print(f"filename: {filename}")
+        filenames.append(filename)
 
-    print(f"Done handling message: {json_decoded}")
+    new_filenames = _do_work(filenames)
+    print(f"\nDone handling message: {json_decoded}")
     return return_val
 
 
-def main(args):
-    decoded_message = sys.argv[1]
-    lambda6_handle_message(decoded_message)
+def main(json_string):
+    """ main gets invoked because the listener does a system call to it.
+        The listener passes to main the json string.
+        Therefore, when testing, make the json string in the if __main__ block.
+    """
+
+    lambda6_handle_message(json_string)
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main(sys.argv)
+    if os.path.isfile(sys.argv[1]):
+        with open(sys.argv[1], 'r') as file:
+            json_string = file.read()
+    else:
+        json_string = sys.argv[1]
+
+    main(json_string)
