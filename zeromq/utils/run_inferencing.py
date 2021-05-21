f0a2e3527ac73490dcc02553fc6cb1ef9e2c962f
brettin <brettin@anl.gov>
2021-05-21 08:29:15 -0500

    run_inferencing

---
+++ zeromq/utils/run_inferencing.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.path.append("../utils/")
+sys.path.append("./utils/")
+sys.path.append("../rdbms/")
+sys.path.append("../../rdbms/")
+sys.path.append("../../../rdbms")
+
+from connect import connect
+# connect to mysql, cnx is global to this file
+cnx = connect()
+
+def run_inferencing(filenames):
+    ''' run model in inferencing mode (predict)
+        params: model file name and sample dataframe
+        return: a file of predictions
+    '''
+
+    print(f'\ninferring using filenames: {filenames}')
+    
+    # stub - implement this when we have a model to train
+    basename = 'basename'
+    with open(basename+'_predictions.csv', 'w') as f:
+        f.write('PASS')
+    new_files = [basename+'_predictions.csv']
+    # end stub implementation
+
+    return new_files
+
+def main(filenames):
+    run_inferencing(filenames)
+    return 'done predicting'
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main([(sys.argv[1])])
