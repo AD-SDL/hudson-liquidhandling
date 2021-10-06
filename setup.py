6992e55fa710e389c0317588772a9676008b2a9c
Ryan Lewis <ryandlewis.rl@gmail.com>
2021-10-06 19:09:00 -0400

    Bumped to v0.1.36

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="liquidhandling",
-    version="0.1.35",
+    version="0.1.36",
     description="A python library developed to allow programmatic control of the Hudson Robotics systems installed in Argonne National Laboratory's Secure BIO Lab. ",
     url="http://github.com/luckierdodge/liquidhandling",
     author="Ryan D. Lewis",
