59742af49f17bbaff3da469e0c941438a9e9f5fd
Ryan Lewis <ryan.lewis@anl.gov>
2022-01-19 08:17:08 -0600

    hudson01 additions for db plate and data entries

--- zeromq/utils/send_data.bat
+++ zeromq/utils/send_data.bat
@@ -3,9 +3,21 @@ echo %DATE% %TIME% >> C:\labautomation\log\send_data.log
 
 :: Check for plate id argument
 IF "%1"=="" (
-    SET plate_id = -1
+    SET /A plate_id = -1
 ) ELSE (
-    SET plate_id = %1
+    SET /A plate_id = %1
 )
 
-powershell.exe -Command "& 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Miniconda3' ; conda activate jupyter ; python 'C:\Users\svcaibio\Dev\liquidhandling\zeromq\hudson01_send_data.py' -d 'C:\labautomation\data' -e '.xlsx' -id %plate_id% >> C:\labautomation\log\hudson01_send_data.log 2>&1
+:: Check for experiment name argument 
+IF "%2"=="" (
+    SET experiment_name=
+) ELSE (
+    SET experiment_name=%2
+)
+
+:: Log command line arguments 
+ECHO %plate_id% >> C:\labautomation\log\send_data.log
+ECHO %experiment_name% >> C:\labautomation\log\send_data.log
+
+:: send data with call to hudson01_send_data.py
+powershell.exe -Command "& 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Miniconda3' ; conda activate jupyter ; python 'C:\Users\svcaibio\Dev\liquidhandling\zeromq\hudson01_send_data.py' -d 'C:\labautomation\data' -e '.xlsx' -id %plate_id% -en %experiment_name% >> C:\labautomation\log\hudson01_send_data.log 2>&1
