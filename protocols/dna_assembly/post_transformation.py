ef181cdb94e74b95871ea3203791cacfd3c7e7ec
Ryan Lewis <ryan.lewis@anl.gov>
2022-03-02 14:57:25 -0600

    post transformation bug fixes, all .hso files tested

--- protocols/dna_assembly/post_transformation.py
+++ protocols/dna_assembly/post_transformation.py
@@ -6,7 +6,6 @@ import time
 from subprocess import Popen
 from liquidhandling import SoloSoft
 from liquidhandling import SoftLinx
-#from liquidhandling import DeepBlock_96VWR_75870_792_sterile
 from liquidhandling import Reservoir_12col_Agilent_201256_100_BATSgroup
 from liquidhandling import Plate_96_Corning_3635_ClearUVAssay
 
@@ -23,24 +22,15 @@ Pos 6 = EMPTY AT START (later 96 well clear, flat-bottom plate w/ lid placed by 
 Pos 7 = EMPTY
 Pos 8 = EMPTY
 
-Stack 5 - 96 well clear, flat-bottom plate w/ lid.  (will be placed on deck pos 4 at start of protocol)
-Stack 4 - Full Tip Box Replacements 
-Stack 3 - Empty Tip Box Storage (empty at start)
-
-Example command line usage: (creating 3 plates)
-python campaign2_loop_dil2x.py -tr col1 col2 col3 -cc 1 2 3 -mc 1 3 5 -tdh 1 2 1 -cdc 1 2 3
-
 Post Transformation Steps: 
-
-
-
-
 """
 
 def generate_post_transformation(): 
 
     # file paths
-    instruction_file_directory = "C:\\labautomation\\instructions"
+    #instruction_file_directory = "C:\\labautomation\\instructions"
+    #instruction_file_directory = "C:\\Users\\svcaibio\\Desktop\\FOR_BEN"
+    instruction_file_directory = "C:\\Users\\svcaibio\\Desktop\\Debug\\post_transformation"
     transf_to_sel_filename = os.path.join(instruction_file_directory, "transf_to_sel.hso")
     sel_to_sel_filename = os.path.join(instruction_file_directory, "sel_to_sel.hso")
     sel_to_master_filename = os.path.join(instruction_file_directory, "sel_to_master.hso")
@@ -57,25 +47,24 @@ def generate_post_transformation():
     # z shift
 
     # default variables 
-    flat_96_z_shift = 1 
+    flat_96_z_shift = 2  # changed from 1 TEST 
     default_num_mix = 3
-    default_mix_volume = 100
+    default_mix_volume = 45 # using 50 uL tips TEST 
 
     # transformation plate to selection plate variables (transformation plate -> selection plate #1)
     transf_plate_wells = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12"]
     sel_plate_wells = transf_plate_wells.copy()
     transf_plate_asp_volume_step_1 = 10
-    media_asp_volume_step_1 = 180
-    transf_plate_mix_volume_step_1 = 50 #?
-    sel_plate_mix_volume_step_1 = 100 #?
+    transf_plate_mix_volume_step_1 = 45 # using 50uL tips 
+    sel_plate_mix_volume_step_1 = 45 
     num_mix_step_1 = 3
 
     # selection plate to selection plate variables
-    sel_to_sel_asp_volume = 1 # check that this can even be done...
+    sel_to_sel_asp_volume = 2 # check that this can even be done...
     # keep the other variables the same as step 1
 
     # step 4 variables (selection plate #3 -> master/freezer plate)
-    sel_to_master_asp_volume = 100
+    sel_to_master_asp_volume = 100 # do in 2 transfers, using 50uL tips!
 
     # step 5 variables (master/freezer plate -> overnight plate)
     master_to_overnight_asp_volume = 10
@@ -86,21 +75,28 @@ def generate_post_transformation():
     # Hidex assay variables 
     hidex_assay_name = "Campaign1_noIncubate2"  # TODO --> make Hidex protocol in app
     num_readings = 6  # total, including T0 reading
-    incubation_time_between_readings = [0,1,0,0] # 1 hour
+    
 
-    # tear down variables
-    selection_incubation_time = [0,3,0,0]  # --> 0 days, 3 hours, 0 minutes, 0 seconds
+    # #incubation times
+    # default_incubation_time = [0,3,0,0]  # --> 0 days, 3 hours, 0 minutes, 0 seconds 
+    # overnight_incubation_time = [0,8,0,0]  # --> 0 days, 8 hours, 0 minutes, 0 seconds
+    # incubation_time_between_readings = [0,1,0,0] # 0 days, 1 hour, 0 mintues, 0 seconds
+
+    # FOR TESTING
+    default_incubation_time = [0,0,0,10]  # --> 0 days, 0 hours, 0 minutes, 10 seconds 
+    overnight_incubation_time = [0,0,0,20] #  --> 0 days, 0 hours, 0 minutes, 20 seconds
+    incubation_time_between_readings = [0,0,0,5] # 0 days, 0 hours, 0 minutes, 5 seconds
 
 
     #* SOFTLINX .slvp PROTOCOL - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-    softLinx = SoftLinx("Post Transformation", "C:\\Users\\svcaibio\\Desktop\\Debug\\post_transformation.slvp")
+    softLinx = SoftLinx("Post Transformation", "C:\\Users\\svcaibio\\Desktop\\Debug\\post_transformation\\post_transformation.slvp")
 
     # initialize plates 
     softLinx.setPlates({"SoftLinx.PlateCrane.Stack5": "Plate.96.Corning-3635.ClearUVAssay"})  # assume all new plates in same stack until stack issue fixed
 
     """ Assume transformation plate is already in incubator with plate ID 0"""
 
-    plate_id= 0  # keep track of current plate id
+    plate_id=1  # keep track of current plate id
     
     #* TRANSFORMATION PLATE --> SELECTION PLATE #1
     # set up SOLO deck
@@ -122,7 +118,7 @@ def generate_post_transformation():
     
     # clear SOLO deck and transfer new plate to incubator
     plate_id += 1
-    tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id, incubation_time=selection_incubation_time, shaker_speed=30)
+    tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id, incubation_time=default_incubation_time, shaker_speed=30)
 
     #* SELECTION PLATE #1 --> SELECTION PLATE #2 --> SELECTION PLATE #3
     # generate the liquidhandling hso (same for both steps)
@@ -146,7 +142,7 @@ def generate_post_transformation():
 
         # clear SOLO deck and move new plate to incubator
         plate_id += 1
-        tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id, incubation_time=[0,3,0,0], shaker_speed=30)
+        tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id, incubation_time=default_incubation_time, shaker_speed=30)
 
     #* SELECTION PLATE #3 --> MASTER PLATE
     # set up the SOLO deck
@@ -182,7 +178,7 @@ def generate_post_transformation():
     # place prefilled media plate onto solo deck position 4, remove lid
     softLinx.plateCraneMovePlate(["SoftLinx.PlateCrane.Stack5"],["SoftLinx.Solo.Position4"])
     softLinx.plateCraneRemoveLid(["SoftLinx.Solo.Position4"],["SoftLinx.Solo.LidNest2"])
-    softLinx.plateCraneMoveCrane(["softLinx.PlateCrane.Safe"])
+    softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
 
     # generate and run liquidhandling hso
     hso_5 = generate_hso(
@@ -200,7 +196,7 @@ def generate_post_transformation():
 
     # clear SOLO deck and move new plate to incubator
     plate_id += 1
-    tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id,incubation_time=[0,8,0,0])
+    tear_down(current_softLinx=softLinx, incubator_plate_id=plate_id,incubation_time=overnight_incubation_time,shaker_speed=30)
 
 
 
@@ -229,13 +225,13 @@ def generate_post_transformation():
     #* TEST PLATE HIDEX READINGS 
     # T0 hidex reading
     softLinx.plateCraneMovePlate(["SoftLinx.Solo.Position4"],["SoftLinx.Hidex.Nest"])
-    softLinx.plateCraneMoveCrane(["SoftLinx.PlateCrane.Safe"])
+    softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
     softLinx.hidexRun(hidex_assay_name)
     #TODO send results to lambda6 automatically
     softLinx.plateCraneMovePlate(["SoftLinx.Hidex.Nest"],["SoftLinx.Liconic.Nest"])  #TODO: define Liconic.NestAfterHidex location
     softLinx.hidexClose()
     softLinx.plateCraneReplaceLid(["SoftLinx.PlateCrane.LidNest2"],["SoftLinx.Liconic.Nest"])
-    softLinx.plateCraneMoveCrane(["SoftLinx.PlateCrane.Safe"])
+    softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
     softLinx.liconicLoadIncubator(loadID=plate_id)
     softLinx.liconicShake(shaker1Speed=30, shakeTime=incubation_time_between_readings)
     
@@ -255,7 +251,7 @@ def generate_post_transformation():
             softLinx.plateCraneMovePlate(["SoftLinx.Hidex.Nest"],["SoftLinx.Liconic.Nest"])  #TODO: define Liconic.NestAfterHidex location
             softLinx.hidexClose()
             softLinx.plateCraneReplaceLid(["SoftLinx.PlateCrane.LidNest2"],["SoftLinx.Liconic.Nest"])
-            softLinx.plateCraneMoveCrane(["SoftLinx.PlateCrane.Safe"])
+            softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
             softLinx.liconicLoadIncubator(loadID=plate_id)
             softLinx.liconicShake(shaker1Speed=30, shakeTime=incubation_time_between_readings)
 
@@ -289,7 +285,7 @@ def set_up(current_softLinx:SoftLinx, incubator_plate_id):
 
 
 # ---------------------------------------------------------------------------------
-def tear_down(current_softLinx:SoftLinx, incubator_plate_id, incubation_time, shaker_speed): 
+def tear_down(current_softLinx:SoftLinx, incubator_plate_id, incubation_time, shaker_speed=30): 
     """ tear_down
     
         Description: Clears the SOLO deck after liquidhandling is complete
@@ -310,7 +306,7 @@ def tear_down(current_softLinx:SoftLinx, incubator_plate_id, incubation_time, sh
     # replace lid on new plate and move to incubator nest 
     current_softLinx.plateCraneReplaceLid(["SoftLinx.PlateCrane.LidNest2"], ["SoftLinx.Solo.Position4"])
     current_softLinx.plateCraneMovePlate(["SoftLinx.Solo.Position4"],["SoftLinx.Liconic.Nest"])
-    current_softLinx.plateCraneMoveCrane(["softLinx.PlateCrane.Safe"])
+    current_softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
 
     # load the new plate into the incubator
     current_softLinx.liconicLoadIncubator(loadID=incubator_plate_id)
@@ -337,7 +333,7 @@ def take_hidex_reading(current_softLinx:SoftLinx, incubator_plate_id, hidex_assa
     # remove lid and transfer to Hidex
     current_softLinx.plateCraneRemoveLid(["SoftLinx.Liconic.Nest"],["SoftLinx.LidNest2"])
     current_softLinx.plateCraneMovePlate(["SoftLinx.Liconic.Nest"],["SoftLinx.Hidex.Nest"])
-    current_softLinx.plateCraneMoveCrane(["SoftLinx.PlateCrane.Safe"])
+    current_softLinx.plateCraneMoveCrane("SoftLinx.PlateCrane.Safe")
 
     # take Hidex reading
     current_softLinx.hidexRun(hidex_assay)
@@ -363,6 +359,11 @@ def generate_hso(file_path, origin_wells, destination_wells, volume, num_mix, or
             destination_z_shift: distance from well bottom (mm) to dispense
     
     """
+    two_transfers = False 
+    if volume > 50: 
+        volume = float(volume)/float(2)
+        two_transfers = True
+
     soloSoft = SoloSoft(
         filename=file_path,
         plateList=[
@@ -370,8 +371,8 @@ def generate_hso(file_path, origin_wells, destination_wells, volume, num_mix, or
             "Empty",
             "TipBox.50uL.Axygen-EV-50-R-S.tealbox", #TODO: do we have 20uL tips? 
             "Plate.96.Corning-3635.ClearUVAssay",
-            "Plate.96.Corning-3635.ClearUVAssay",
             "Empty",
+            "Plate.96.Corning-3635.ClearUVAssay",
             "Empty",
             "Empty",
         ],
@@ -389,14 +390,34 @@ def generate_hso(file_path, origin_wells, destination_wells, volume, num_mix, or
             dispense_height=origin_z_shift,
         )
         soloSoft.dispense(
-            positon="Position4", 
-            dispense_volumes=Plate_96_Corning_3635_ClearUVAssay.setCell(destination_wells[i][0], int(destination_wells[i][1:]), volume),
+            position="Position4", 
+            dispense_volumes=Plate_96_Corning_3635_ClearUVAssay().setCell(destination_wells[i][0], int(destination_wells[i][1:]), volume),
             dispense_shift=[0,0,destination_z_shift],
             mix_at_finish=True, 
             mix_cycles=num_mix, 
             mix_volume=destination_mix_volume, 
             aspirate_height=destination_z_shift,
         )
+
+        if two_transfers == True: # repeat the above transfers again to transfer total volume
+            soloSoft.aspirate(
+                position="Position6", 
+                aspirate_volumes=Plate_96_Corning_3635_ClearUVAssay().setCell(origin_wells[i][0], int(origin_wells[i][1:]), volume),
+                aspirate_shift=[0,0,origin_z_shift], 
+                mix_at_start=True, 
+                mix_cycles=num_mix, 
+                mix_volume=origin_mix_volume,
+                dispense_height=origin_z_shift,
+            )
+            soloSoft.dispense(
+                position="Position4", 
+                dispense_volumes=Plate_96_Corning_3635_ClearUVAssay().setCell(destination_wells[i][0], int(destination_wells[i][1:]), volume),
+                dispense_shift=[0,0,destination_z_shift],
+                mix_at_finish=True, 
+                mix_cycles=num_mix, 
+                mix_volume=destination_mix_volume, 
+                aspirate_height=destination_z_shift,
+            )
 
     soloSoft.shuckTip()
     soloSoft.savePipeline()
