fef48d603540f865dee795ec223bd6a34c00e0e1
brettin <brettin@anl.gov>
2022-03-12 09:26:57 -0600

    adding PING

--- zeromq/lambda6_listen.py
+++ zeromq/lambda6_listen.py
@@ -11,12 +11,19 @@ Lambda6 listens for data files from the Hidex (Port 5555)
 import os
 import sys
 import zmq
-import time
+import datetime
 import json
 import _thread
 import threading
 from subprocess import Popen
 from lambda6_handle_message import lambda6_handle_message
+
+# This should probably go into a core module so that we have common
+# timestamps across the system
+def timestamp():
+    date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    return date
+
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -28,11 +35,18 @@ while True:
 
     if message == b"SHUTDOWN":
         socket.send(b"Shutting down")
+        print (timestamp() + ": received SHUTDOWN, shutting down.")
         break
+
+    elif message == b"PING":
+        response = timestamp() + ": echo"
+        socket.send(response.encode())
 
     else:
         # immediately pass the message off to message handler and keep listening
+        # this needs to change from a systen call to a thread pool
         child_message_handler = Popen(
             ["python", "./lambda6_handle_message.py", decoded], start_new_session=True
         ).pid
+        date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
         socket.send(b"Message received and passed to lambda6_handle_message")
