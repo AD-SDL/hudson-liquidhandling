f0db41028193c32840428e3db4a0234420e57e90
Casey Stone <cstone@anl.gov>
2022-04-06 22:52:23 -0500

    removing db call for testing

--- zeromq/lambda6_send_instructions.py
+++ zeromq/lambda6_send_instructions.py
@@ -57,8 +57,8 @@ def lamdba6_send_instructions(instructions_dir, info_list):
             num_plates, num_wells, plate_type, directory_name = info.split(",")
             directory_name = directory_name[:-1]
             
-            # insert call to database here
-            create_empty_plate_records(int(num_plates), int(num_wells), plate_type, directory_name,is_test)
+            # insert call to database here TESTING
+            #create_empty_plate_records(int(num_plates), int(num_wells), plate_type, directory_name,is_test)
 
             # archive instructions once sent correctly
             archive([instructions_dir], "/lambda_stor/data/hudson/instructions/")
