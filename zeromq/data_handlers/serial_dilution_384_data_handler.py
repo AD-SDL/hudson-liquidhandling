256f22ca0aa51537f74f7a7b6cebbb63f6621a65
priyanka <pvsetty@uchicago.edu>
2022-08-04 12:24:49 -0500

    graphing function added to data handler

--- zeromq/data_handlers/serial_dilution_384_data_handler.py
+++ zeromq/data_handlers/serial_dilution_384_data_handler.py
@@ -52,6 +52,9 @@ def handle_sd_384_data(address, json_decoded_message):
             data_basename = data_filename.split(".")[0]
             j+=1
 
+            graph_filepaths_list = generate_graphs(blank_adj_df, data_filename, plot_dir_path,i)  # REMOVED FOR TESTING
+            print(f"graphs generated for {data_basename}")
+
             if j == 4:
                 print(f"Done handling message: {str(address)}")
 
@@ -331,4 +334,98 @@ def od_blank_adjusted(data_frame, time_stamps,i):
     
     return blank_adj_data_frame, adjusted_values_list
 
-    # TO DO: graphing
+    
+def generate_graphs(blank_adj_df, data_filename, plot_directory_path, quadrant): #TODO: check graph format
+    """generate_graphs
+
+    Description: Received a blank adjusted data frame and produces one graph per data timepoint
+
+    Parameters: 
+        blank_adj_df: blank adjusted data in data frame format
+        data_filename: file name of the original data csv file without extension (not file path)
+        plot_directory_path: path to the directory where all created graphs should be stored 
+
+    Returns: 
+        plot_paths: a list of file paths to newly saved graphs in order of increasing data timepoint
+    
+    """
+    plot_paths = []
+
+    # define x-axis and x-axis ticks
+    x_axis = []
+    x_rep1 = [1,2,3,4,5,6]
+    x_rep2 = [7,8,9,10,11,12]
+
+    x_ticks = x_rep1.copy()
+    for x in x_rep2:
+        x_ticks.append(x)
+
+    for i in range(8): 
+        x_axis.append(x_rep1)
+        x_axis.append(x_rep2)
+    
+    # define plot colors and figure dimensions
+    colors = ["b","g","r","c","m","y",'tab:brown', "k"]
+
+    # Graph data from each timepoint
+    for timepoint in blank_adj_df.columns[3:]:
+        data_list = blank_adj_df[timepoint].tolist()
+        data_list = [float(x) for x in data_list]
+
+        # determine plot title and file path
+        plot_title = f"{data_filename}, timepoint = {timepoint} (seconds), quadrant = {quadrant+1}"
+        plot_basename = f"{data_filename}_{str(timepoint).split('.')[0]}.png"
+        
+        try:
+            plot_file_path = os.path.join(plot_directory_path, plot_basename)
+            plot_paths.append(plot_file_path)
+        except OSError as e: 
+            print(e)
+
+        data_in_sets = []
+        data_rep1 = []
+        data_rep2 = []
+
+# TO DO: figure out the indices
+        # # separate data into replicate 1 and 2
+        # while not len(data_list) == 0:  
+        #     data_in_sets.append(data_list[:6])
+        #     data_in_sets.append(data_list[6:12])
+        #     data_rep1.append(data_list[:6])
+        #     data_rep2.append(data_list[6:12])
+        #     data_list = data_list[12:]
+
+        plt.figure(figsize=(10, 5))
+
+        # plot each replicate separately (to maintain color across replicates)
+        for i in range(len(data_rep1)): 
+            plt.plot(x_rep1, data_rep1[i], colors[i])
+        for i in range(len(data_rep2)): 
+            plt.plot(x_rep2, data_rep2[i], colors[i])
+
+        plt.legend(["Row A and B","Row C and D","Row E and F","Row G and H", "Row I and J", "Row K and L", "Row M and N", "Row O and P"], loc='center left', bbox_to_anchor=(1, 0.5), borderaxespad=0.)  #TODO: extract strain names from db
+        plt.title(plot_title)
+        plt.xticks(x_ticks)
+        plt.xlabel("Dilution columns with highest to lowest concentration from left to right with last column being control")
+        plt.ylabel("Blank-adjusted OD(590)")
+    
+        # save figure to file
+        plt.savefig(plot_file_path)
+        plt.close() 
+
+    return plot_paths
+
+
+def main(json_string):
+    lambda6_handle_message(json_string)
+    
+
+if __name__ == "__main__":
+    #execute only if run as a script
+    if os.path.isfile(sys.argv[1]):
+        with open(sys.argv[1], "r") as file:
+            json_string = file.read()
+    else:
+        json_string = sys.argv[1]
+
+    main(json_string)
